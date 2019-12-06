998ccf692dc58152b194a74d8061571a486bcd6f
Hemanth Reddy Galigutta <51997942+ghreddy222@users.noreply.github.com>
2019-12-06 15:43:27 +0530

    Add files via upload

---
+++ Record/Palind.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Palind
+{
+ public static void main (String args[])
+{
+  String a=args[0];
+String b="";
+	int i;
+  System.out.print(args[0]);
+   int len=a.length();
+for(i=len-1;i>=0;i--)
+  
+   b=b+a.charAt(i);
+   if (a.equals(b))
+   
+    System.out.print("is a Palindrome");
+    
+    else
+     System.out.print("is not a Not palindrome");
+}}
---
+++ obs/Cl.java
@@ -0,0 +1,14 @@
+class Cl
+{
+public static void main(String args[])
+{
+
+s:for (int i=1;i<=5;i++)
+{
+if (i==5)
+{
+break s;
+}
+System.out.print(i);
+
+}}}
